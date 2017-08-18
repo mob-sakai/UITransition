73ee64cab40df75be4284b30bef5394ff4f5208e
Takashi Sakai <sakai@mobcast.jp>
2017-08-18 15:39:27 +0900

    Automatically export unitypackage

---
+++ Assets/Editor/ExportPackage.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+
+namespace Mobcast.Coffee
+{
+	public static class ExportPackage
+	{
+		const string kPackageName = "UITransition.unitypackage";
+		static readonly string[] kAssetPathes = {
+			"Assets/Mobcast/Coffee/UITransition",
+		};
+
+		[MenuItem ("Export Package/" + kPackageName)]
+		[InitializeOnLoadMethod]
+		static void Export ()
+		{
+			if (EditorApplication.isPlayingOrWillChangePlaymode)
+				return;
+			
+			AssetDatabase.ExportPackage (kAssetPathes, kPackageName, ExportPackageOptions.Recurse | ExportPackageOptions.Default);
+			UnityEngine.Debug.Log ("Export successfully : " + kPackageName);
+		}
+	}
+}
